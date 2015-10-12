9e9793c3879ea4407be4133674dcea37e9c692ef
Chris <chris@usablehq.com>
2015-10-12 08:46:24 +0100

    Dynamic slider creation

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
+
 var connectionId = -1;
+var MY_HID_VENDOR_ID = 8352;
+var MY_HID_PRODUCT_ID = 16869;
+var DEVICE_INFO = {"vendorId": MY_HID_VENDOR_ID, "productId": MY_HID_PRODUCT_ID };
 
-window.onload = function() {
+window.onload = function() { initDeviceConnection(); };
 
-  var MY_HID_VENDOR_ID = 8352;
-  var MY_HID_PRODUCT_ID = 16869;
-  var DEVICE_INFO = {"vendorId": MY_HID_VENDOR_ID, "productId": MY_HID_PRODUCT_ID };
+function slider(){
+
+  var i = this.getAttribute('rel');
   
+  var r = document.querySelector("input.red[rel='"+i+"']").value;
+  var g = document.querySelector("input.green[rel='"+i+"']").value;
+  var b = document.querySelector("input.blue[rel='"+i+"']").value;
+
+  setColor(i,r,g,b);
+
+}
+
+function setColor(i,r,g,b) {
+  
+  if (connectionId) {
+
+    var data = [0, i, r, g, b];
+    var arr = new Uint8Array(data);
+  
+    chrome.hid.sendFeatureReport( connectionId, 5, arr.buffer, function(){
+      //console.log('Set color');
+    });
+
+  }
+
+}
+
+chrome.hid.onDeviceAdded.addListener(function(){
+
+  console.log('Blinkstick added');
+  initDeviceConnection();
+
+});
+
+chrome.hid.onDeviceRemoved.addListener(function(){
+
+  console.log('Blinkstick removed');
+
+});
+
+function initDeviceConnection(){
+
   chrome.hid.getDevices(DEVICE_INFO, function(devices) {
 
     if (!devices || !devices.length) {
@@ -17,54 +59,58 @@ window.onload = function() {
 
     chrome.hid.connect(hidDevice, function(connection) {
        connectionId = connection.connectionId;
-     });
+    });
   
   });
+
+  for (var x = 0; x < 2; x++) {
+
+    var template = sliderTemplate(x);
+    $('body').append(template);
+
+  }
+
+  var sliders = document.querySelectorAll("input[type=range]");
+  for (var s = 0; s < sliders.length; s++) {
+    sliders[s].addEventListener('input', slider);
+  }
+
   
-};
-
-document.querySelector("#off").addEventListener('click', function(){
-
-  setColor(0,0,0,0);
-
-});
-
-document.querySelector("#redSlider").addEventListener('input', slider);
-document.querySelector("#greenSlider").addEventListener('input', slider);
-document.querySelector("#blueSlider").addEventListener('input', slider);
-
-function slider(){
-
-  var i = document.querySelector("input[name=channel]:checked").value;
-
-  var r = document.querySelector("#redSlider").value;
-  var g = document.querySelector("#greenSlider").value;
-  var b = document.querySelector("#blueSlider").value;
-
-  setColor(i,r,g,b);
-
 }
 
-function setColor(i,r,g,b) {
+function sliderTemplate(i) {
 
-  var data = [0, i, r, g, b];
-  var arr = new Uint8Array(data);
+  var template = '<div class="col-sm-2">'+
+    '<div class="panel panel-default">'+
+    '  <div class="panel-heading">'+
+    '    <h3 class="panel-title">LED '+i+' Control</h3>'+
+    '  </div>'+
+    '  <div class="panel-body">'+
 
-  chrome.hid.sendFeatureReport( connectionId, 5, arr.buffer, function(){
-    //console.log('Set color');
-  });
+    '      <div class="row">'+
+    '        <div class="col-xs-3" style="text-align: center;">'+
+    '          <input class="red" rel="'+i+'" type="range" min=0 max=255 step=1 value=0 style="height: 200px;" orient="vertical" />'+
+    '          R'+
+    '        </div>'+
+    '        <div class="col-xs-3" style="text-align: center;">'+
+    '          <input class="green" rel="'+i+'" type="range" min=0 max=255 step=1 value=0 style="height: 200px;" orient="vertical" />'+
+    '          G'+
+    '        </div>'+
+    '        <div class="col-xs-3" style="text-align: center;">'+
+    '          <input class="blue" rel="'+i+'" type="range" min=0 max=255 step=1 value=0 style="height: 200px;" orient="vertical" />'+
+    '          B'+
+    '        </div>'+
+    '        <div class="col-xs-3" style="text-align: center;">'+
+    '          <input class="lum" rel="'+i+'" type="range" min=0 max=255 step=1 value=0 style="height: 200px;" orient="vertical" />'+
+    '          F'+
+    '        </div>'+
+    '      </div>'+
 
+    '  </div>'+
+    ' </div>'+
+    '</div>';
 
+    return template;
+  
 }
 
-chrome.hid.onDeviceAdded.addListener(function(){
-
-  console.log('Blinkstick added');
-
-});
-
-chrome.hid.onDeviceRemoved.addListener(function(){
-
-  console.log('Blinkstick removed');
-
-});
